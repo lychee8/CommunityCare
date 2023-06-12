98caeb5902ab4f6dda88eecfc8941e975d162f57
Giselle Ng <gigieng80@gmail.com>
2023-06-12 12:19:25 -0700

    Initial Commit

---
+++ CommunityCare/CommunityCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityCareApp.swift
+//  CommunityCare
+//
+//  Created by G Eng on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CommunityCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
