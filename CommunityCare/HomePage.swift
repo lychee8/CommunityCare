7091769a75b05049922651e6dee58ea4822ed8f4
Giselle Ng <gigieng80@gmail.com>
2023-06-14 11:39:27 -0700

    Home Screen (Scroll) V.2

--- CommunityCare/HomePage.swift
+++ CommunityCare/HomePage.swift
@@ -16,52 +16,54 @@ struct HomePage: View {
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
                 
-                VStack{
-                    Text("Community Care")
-                        .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                        .foregroundColor(.black)
-                    
-                    
-                        .padding()
-                        .background(Rectangle().foregroundColor(Color(hue: 0.524, saturation: 0.200, brightness: 0.947)))
-                        .padding()
-                    
-                    
-                        .padding()
-                    Text("Welcome to Community Care! Here you can find food banks and small businesses that you can support in your area. If you are coordinating an event, we also have a To-Do tab to help you keep track of everything.")
-                        .font(.callout)
-                        .padding()
-                    
-                        .padding()
-                        .background(Rectangle().foregroundColor(Color(hue: 0.524, saturation: 0.200, brightness: 0.947)))
-                        .padding()
-                    
-                    
-                    Spacer()
+                ScrollView {
+                
+                    VStack (spacing: 20){
+                        Text("Community Care")
+                            .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.black)
                         
-                        .toolbar{
-                            ToolbarItemGroup(placement: .status){
-                                NavigationLink(destination: FoodBankPage())
-                                {Text("Food Bank")
-                                        .font(.body)
+                        
+                            .padding()
+                            .background(Rectangle().foregroundColor(Color(hue: 0.524, saturation: 0.200, brightness: 0.947)))
+                            .padding()
+                        
+                        
+                            .padding()
+                        Text("Welcome to Community Care! Here you can find food banks and small businesses that you can support in your area. If you are coordinating an event, we also have a To-Do tab to help you keep track of everything.")
+                            .font(.callout)
+                            .padding()
+                        
+                            .padding()
+                            .background(Rectangle().foregroundColor(Color(hue: 0.524, saturation: 0.200, brightness: 0.947)))
+                            .padding()
+                        
+                        
+                        Spacer()
+                        
+                            .toolbar{
+                                ToolbarItemGroup(placement: .status){
+                                    NavigationLink(destination: FoodBankPage())
+                                    {Text("Food Bank")
+                                            .font(.body)
+                                    }
                                 }
                             }
-                        }
-                        .toolbar{
-                            ToolbarItemGroup(placement: .status){
-                                NavigationLink(destination: SmallBizPage())
-                                {Text("Small Businesses")
-                                        .font(.body)
+                            .toolbar{
+                                ToolbarItemGroup(placement: .status){
+                                    NavigationLink(destination: SmallBizPage())
+                                    {Text("Small Businesses")
+                                            .font(.body)
+                                    }
+                                }
+                            }.toolbar{
+                                ToolbarItemGroup(placement: .status){
+                                    NavigationLink(destination: ToDoPage())
+                                    {Text("To-Do")
+                                            .font(.body)
+                                    }
                                 }
                             }
-                        }.toolbar{
-                            ToolbarItemGroup(placement: .status){
-                                NavigationLink(destination: ToDoPage())
-                                {Text("To-Do")
-                                        .font(.body)
-                                }
-                            }
-                        }
                     }
                 }
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
@@ -69,7 +71,7 @@ struct HomePage: View {
                 
             }
         }
-           
+    }
     }
         
 
