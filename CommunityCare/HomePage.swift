0ac8a610d2511401f21a8a1424ff7af3e2171f67
Giselle Ng <gigieng80@gmail.com>
2023-06-13 18:30:49 -0700

    Creation of Home Page

---
+++ CommunityCare/HomePage.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  CommunityCare
+//
+//  Created by G Eng on 6/13/23.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text("Community Care")
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
