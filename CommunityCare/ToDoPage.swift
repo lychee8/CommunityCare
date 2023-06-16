5496f206e67e29b392d56b0baff2831ddb87be6e
Eliz <hsu.eliz@gmail.com>
2023-06-16 08:30:59 -0700

    To Do

--- CommunityCare/ToDoPage.swift
+++ CommunityCare/ToDoPage.swift
@@ -10,11 +10,6 @@ import SwiftUI
 struct ToDoPage: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ToDoPage_Previews: PreviewProvider {
-    static var previews: some View {
-        ToDoPage()
+        Text("Coming Soon!")
     }
 }
